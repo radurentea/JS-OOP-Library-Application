e584d8e4656ba73c7b7a5e31e77258002c5e5dbc
Radu Rentea <radu.rentea@gmail.com>
2019-05-18 18:10:33 +0300

    add checkout method

--- Patron.js
+++ Patron.js
@@ -6,4 +6,15 @@ class Patron
         this.email = email;
         this.currentBook = null;
     }
+    checkout(book)
+    {
+        this.currentBook = book;
+        book.out = true;
+        book.patron = this;
+
+        const newDueDate = new Date();
+        newDueDate.setDate(newDueDate.getDate() + 14);
+        book.dueDate = newDueDate;
+    }
+
 }

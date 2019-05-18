36dc9821bc53fddb44bc56680c639ba4fc170ad1
Radu Rentea <radu.rentea@gmail.com>
2019-05-18 18:11:05 +0300

    add returnBook method

--- Patron.js
+++ Patron.js
@@ -16,5 +16,11 @@ class Patron
         newDueDate.setDate(newDueDate.getDate() + 14);
         book.dueDate = newDueDate;
     }
-
+    returnBook(book)
+    {
+        this.currentBook = null;
+        book.out = false;
+        book.patron = null;
+        book.dueDate = null;
+    }
 }

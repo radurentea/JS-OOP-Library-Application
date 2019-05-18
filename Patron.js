9a164d49be64a86cecc548812073f8940df4889f
Radu Rentea <radu.rentea@gmail.com>
2019-05-18 18:25:06 +0300

    Remove dueDate code, move it as setter to Book.js

--- Patron.js
+++ Patron.js
@@ -11,16 +11,11 @@ class Patron
         this.currentBook = book;
         book.out = true;
         book.patron = this;
-
-        const newDueDate = new Date();
-        newDueDate.setDate(newDueDate.getDate() + 14);
-        book.dueDate = newDueDate;
     }
     returnBook(book)
     {
         this.currentBook = null;
         book.out = false;
         book.patron = null;
-        book.dueDate = null;
     }
 }

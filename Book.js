7d97ad75061d6e70abe8b6b517bf148e6b146232
Radu Rentea <radu.rentea@gmail.com>
2019-05-18 18:23:18 +0300

    Add setter and getter

--- Book.js
+++ Book.js
@@ -5,8 +5,27 @@ class Book
         this.title = title;
         this.author = author;
         this.isbn = isbn;
-        this.out = false;
+        this._out = false;
         this.dueDate = null;
         this.patron = null;
     }
+    set out(out)
+    {
+        this._out = out;
+
+        if (out)
+        {
+            const newDueDate = new Date();
+            newDueDate.setDate(newDueDate.getDate() + 14);
+            this.dueDate = newDueDate;
+        } else
+        {
+            this.dueDate = null;
+        }
+    }
+    get out()
+    {
+        return this._out;
+    }
+
 }

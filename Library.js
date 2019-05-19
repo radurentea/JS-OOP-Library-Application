234c77d467fe4d4b10d68974c8afc4b218526dd4
Radu Rentea <radu.rentea@gmail.com>
2019-05-19 07:07:00 +0300

    Add chargeFines() method

--- Library.js
+++ Library.js
@@ -14,4 +14,17 @@ class Library
     {
         this.books.push(book);
     }
+    chargeFines()
+    {
+        const now = new Date();
+
+        const latePatrons = this.patrons.filter(patron => (patron.currentBook !== null && patron.currentBook.dueDate < now));
+
+        for (let patron of latePatrons)
+        {
+            const dateDiff = new Date(now - patron.currentBook.dueDate);
+            const daysLate = dateDiff.getDate();
+            patron.balance += this.dailyFine * daysLate;
+        }
+    }
 }
